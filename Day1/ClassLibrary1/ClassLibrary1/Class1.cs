748bdf8e8b32e58ebbb048d12033a395d2ea0696
Liam Jones <me@liamjones.me>
2022-12-01 12:20:32 +0000

    day1

---
+++ Day1/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,37 @@
+﻿namespace ClassLibrary1
+{
+    public static class Class1
+    {
+        public static int GetAnswer1(string input)
+        {
+            var elves = GetElves(input);
+
+            return elves.Max();
+        }
+
+        public static int GetAnswer2(string input)
+        {
+            var elves = GetElves(input);
+
+            return elves.OrderByDescending(x => x).Take(3).Sum();
+        }
+
+        private static IEnumerable<int> GetElves(string input)
+        {
+            var elf = 0;
+            var elves = new List<int>();
+            foreach (var line in input.Split(Environment.NewLine))
+            {
+                if (line == "")
+                {
+                    elves.Add(elf);
+                    elf = 0;
+                    continue;
+                }
+                elf += int.Parse(line);
+            }
+            elves.Add(elf);
+            return elves;
+        }
+    }
+}
